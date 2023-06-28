8c13f7a184561bad9eb23e90cc6cca0731c8beb4
Anastasiia <soboanastasiya@yandex.ru>
2023-06-28 14:09:22 +0300

    вносит правки

--- gulpfile.js
+++ gulpfile.js
@@ -86,7 +86,7 @@ export function copyAssets () {
   return gulp.src([
     'source/fonts/**/*.{woff2,woff}',
     'source/*.ico',
-    'source/*.webmanifest',
+    'source/*site.webmanifest',
   ], {
     base: 'source'
   })
--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,52 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-nav__list');
+const navToggle = document.querySelector('.main-header__toggle');
+const navToggleNoJs = document.querySelector('.main-header__toggle--no-js');
+
+navToggle.classList.remove('main-header__toggle--no-js');
+navMain.classList.add('main-nav__list--closed');
+navMain.classList.remove('main-nav__list--is-open');
+navMain.classList.remove('main-nav__list--no-js');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav__list--closed')) {
+    navMain.classList.remove('main-nav__list--closed');
+    navMain.classList.add('main-nav__list--is-open');
+    navToggle.classList.add('main-header__toggle--closed');
+  } else {
+    navMain.classList.add('main-nav__list--closed');
+    navMain.classList.remove('main-nav__list--is-open');
+    navToggle.classList.remove('main-header__toggle--closed');
+  }
+});
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper__pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper__button--next',
+    prevEl: '.swiper__button--prev',
+  },
+});
+
+var map = L.map('map').setView([59.968318, 30.317224], 17.61);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var myIcon = L.icon({
+  iconUrl: 'img/svg/map.svg',
+  iconSize: [38, 50],
+  iconAnchor: [19, 50],
+  // popupAnchor: [-3, -76],
+});
+
+
+L.marker([59.968318, 30.317224], {icon: myIcon}).addTo(map);
